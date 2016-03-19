0c0fec71f5180edd86f82cbdc49407d4e8d64293
sasha <rozalucksemburg@xaker.ru>
2016-03-19 15:30:52 +0300

    script change

--- Cubic_grid/conformal_sphere_grid_sasha/run_intel.sh
+++ Cubic_grid/conformal_sphere_grid_sasha/run_intel.sh
@@ -61,11 +61,24 @@ EOF' > grid/plotscript.sh
 chmod 755 grid/plotscript.sh
 
 
-mpiifort geometry.f90 conformal.f90 matmul.f90 simple_rotations.f90 functions.f90 functions2.f90 matrix_rotation.f90 morphism.f90 grid_generator.f90 data_analyzer.f90 spherical.f90 main.f90 
+mpiifort geometry.f90 conf1.f90 matmul.f90 simple_rotations.f90 functions.f90 functions2.f90 matrix_rotation.f90 morphism.f90 grid_generator.f90 data_analyzer.f90 spherical.f90 main.f90 2> err.file
 # /home/sasha/Fortran/Comands/./compo geometry.o conformal.o matmul.o morphism.o grid_generator.o data_analyzer.o spherical.o main.o
-/home/sasha/Fortran/Comands/./run 1
 
-cd analyze
-./plotscript.sh
-cd ../grid
-./plotscript.sh
+
+if [[ `grep -c error err.file` > 0 ]]; then
+	echo "Look for" `grep -c error err.file` "errors"
+	echo `grep -c warning err.file` "warnings"
+else
+
+	echo `grep -c error err.file` "errors"
+	echo `grep -c warning err.file` "warnings"
+
+	/home/sasha/Fortran/Comands/./run 1
+
+	cd analyze
+	./plotscript.sh
+	cd ../grid
+	./plotscript.sh
+fi
+
+
